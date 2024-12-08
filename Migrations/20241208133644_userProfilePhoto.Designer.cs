8f1efa86c875d27f777c4284bf9947c3239d4da6
Michal Niedzwiecki <michal.niedzwiecki@workai.com>
2024-12-08 14:53:08 +0100

    user avatar beta, graphical difficulty level, dark mode implemented

--- Migrations/20241208133644_userProfilePhoto.Designer.cs
+++ Migrations/20241208133644_userProfilePhoto.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using mPole.Data;
 
@@ -11,9 +12,11 @@ using mPole.Data;
 namespace mPole.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241208133644_userProfilePhoto")]
+    partial class userProfilePhoto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Utils/Validators/MoveValidator.cs
+++ Utils/Validators/MoveValidator.cs
@@ -7,7 +7,7 @@ public class MoveValidator : AbstractValidator<Move>
     {
         RuleFor(move => move.Name).NotEmpty().WithMessage("Nazwa jest obowi¹zkowa");
         RuleFor(move => move.Name).MaximumLength(20).When(move => !string.IsNullOrEmpty(move.Name)).WithMessage("Nazwa nie mo¿e byæ d³u¿sza ni¿ 20 znaków");
-        RuleFor(move => move.DifficultyLevel).InclusiveBetween(1, 3).WithMessage("Poziom trudnoœci musi byæ z zakresu od 1 do 3");
+        RuleFor(move => move.DifficultyLevel).InclusiveBetween(1, 5).WithMessage("Poziom trudnoœci musi byæ z zakresu od 1 do 5");
         RuleFor(move => move.Description).NotEmpty().WithMessage("Opis jest obowi¹zkowy");
         RuleFor(move => move.Images).NotEmpty().WithMessage("Musisz dodaæ przynajmniej jedno zdjêcie figury");
     }
